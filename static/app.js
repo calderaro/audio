cf31f21fd36029018b8899ea1f398e56aa9b54f6
Angel Calderaro <calderaro94@gmail.com>
2017-02-15 02:14:45 -0400

    test1

--- static/app.js
+++ static/app.js
@@ -3,6 +3,7 @@ const audio = document.querySelector('audio')
 const play = document.querySelector('#play')
 const pause = document.querySelector('#pause')
 const next = document.querySelector('#next')
+const next1 = document.querySelector('#next1')
 
 play.addEventListener('click', () => {
   audio.src = '/static/estocolmo.mp3'
@@ -13,6 +14,10 @@ pause.addEventListener('click', () => {
 })
 next.addEventListener('click', () => {
   audio.src = 'https://fonorva.blob.core.windows.net/preupload/FN08040010840_06.mp3'
+  audio.play()
+})
+next1.addEventListener('click', () => {
+  audio.src = '/static/01.mp3'
   audio.play()
 })
 
